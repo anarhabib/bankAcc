d836a495aa59219d6d9a5c6f45cae73e8c2e3d55
Anar Habib <anarhabibullayev3@gmail.com>
2023-09-11 12:20:13 +0400

    deposite and withdraw funcs created

--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ const name = document.getElementById("account");
 const balance = document.getElementById("balance");
 const accDatas = document.getElementById("accDatas");
 
-const deposite = document.getElementById("deposite").value;
-const withdraw = document.getElementById("withdraw").value;
+const deposite = document.getElementById("deposite");
+const withdraw = document.getElementById("withdraw");
 const confrimDepositeBtn = document.getElementById("confrimDeposite");
 const confrimWithdrawBtn = document.getElementById("confrimWithdraw");
 
 const actionsModal = document.getElementById("actions");
 const loginModal = document.getElementById("login");
 
-const selectedAcc = null;
+let selectedAcc = null;
 
 function BankAccount(ownerName, initialBalance) {
   this.ownerName = ownerName;
-  this.balance = initialBalance;
+  this.balance = +initialBalance;
 }
 
 BankAccount.prototype.deposite = function (deposite) {
   if (typeof deposite !== "number" && deposite < 0) {
     throw new Error("Enter valid number!");
   }
-  this.balance += deposite;
+  this.balance += +deposite;
 };
 
 BankAccount.prototype.withdraw = function (withdraw) {
@@ -47,14 +47,35 @@ confrimBtn.addEventListener("click", () => {
   if (!accName && !accBalance) {
     throw new Error("There is empty places!");
   }
-  updateUserData();
   actionsModal.classList.remove("d-none");
   loginModal.classList.add("d-none");
   const newAcc = new BankAccount(accName, accBalance);
   selectedAcc = newAcc;
+  updateUserData();
+
 });
 
 const updateUserData = () => {
   accDatas.innerHTML =
     selectedAcc.ownerName + "'s balance is: " + selectedAcc.balance;
 };
+
+confrimDepositeBtn.addEventListener("click" , () => {
+    const depositeAmount = +deposite.value;
+    if(!depositeAmount){
+        throw new Error("Empty place!")
+    }
+    selectedAcc.deposite(depositeAmount);
+    updateUserData();
+    deposite.value = null;
+})
+
+confrimWithdrawBtn.addEventListener("click" , () => {
+    const withdrawAmount = +withdraw.value;
+    if(!withdrawAmount){
+        throw new Error("Empty place!")
+    }
+    selectedAcc.withdraw(withdrawAmount);
+    updateUserData();
+    withdraw.value = null;
+})
